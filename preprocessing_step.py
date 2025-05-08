8aa246908bfe633f55599d851c9ea12172d758cf
Aleksandra Lubicka <a.lubicka@student.uw.edu.pl>
2025-05-08 22:02:06 +0200

    add

---
+++ preprocessing_step.py
@@ -0,0 +1,5 @@
+import pandas as pd
+import numpy as np
+
+df = pd.read_csv('/Volumes/mlops_dev/olalubic/alubiss/hotel_reservations.csv')
+print(df.head())
--- test_connect.py
+++
@@ -1,4 +0,0 @@
-import numpy as np
-
-print("Testing connect.py")
-print(np.array([1, 2, 3]))
