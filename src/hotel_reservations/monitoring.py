fd41ce45abf9002f3175b2b35058cf029bd23361
alubiss <alubiss>
2025-06-12 19:55:18 +0000

    add

--- src/hotel_reservations/monitoring.py
+++ src/hotel_reservations/monitoring.py
@@ -9,8 +9,9 @@ from databricks.sdk.service.catalog import (
 from loguru import logger
 from pyspark.sql import SparkSession
 from pyspark.sql import functions as F
-from pyspark.sql.functions import monotonically_increasing_id
 from pyspark.sql.types import ArrayType, DoubleType, IntegerType, StringType, StructField, StructType
+from pyspark.sql.functions import lit, when, row_number, col
+from pyspark.sql.window import Window
 
 from hotel_reservations.config import ProjectConfig, Tags
 
@@ -26,8 +27,11 @@ def create_or_refresh_monitoring(config: ProjectConfig, spark: SparkSession, wor
     :param spark: Spark session used for executing SQL queries and transformations.
     :param workspace: Workspace object used for managing quality monitors.
     """
+
     tags = Tags(**{"git_sha": "abcd12345", "branch": "week2", "job_run_id": "1234567890"})
-    inf_table = spark.sql(f"SELECT * FROM {config.catalog_name}.{config.schema_name}.`model-serving-payload_payload`")
+    inf_table = spark.sql(
+        f"SELECT * FROM mlops_dev.olalubic.`model-serving-payload_payload`"
+    )
 
     request_schema = StructType(
         [
@@ -62,71 +66,84 @@ def create_or_refresh_monitoring(config: ProjectConfig, spark: SparkSession, wor
         ]
     )
 
-    response_schema = StructType(
-        [
-            StructField("Client_ID", StringType(), True),
-            StructField("Proba", DoubleType(), True),
-            StructField("Comment", StringType(), True),
-            StructField(
-                "databricks_output",
-                StructType(
-                    [StructField("trace", StringType(), True), StructField("databricks_request_id", StringType(), True)]
-                ),
-                True,
+    response_schema = StructType([
+        StructField(
+            "predictions",
+            ArrayType(
+                StructType([
+                    StructField("Client_ID", StringType(), True),
+                    StructField("Proba", DoubleType(), True),
+                    StructField("Comment", StringType(), True),
+                ])
             ),
-        ]
-    )
+            True
+        ),
+        StructField(
+            "databricks_output",
+            StructType([
+                StructField("trace", StringType(), True),
+                StructField("databricks_request_id", StringType(), True),
+            ]),
+            True
+        ),
+    ])
+
 
     df_parsed = inf_table.withColumn("parsed_request", F.from_json(F.col("request"), request_schema))
     df_exploded = df_parsed.withColumn("record", F.explode(F.col("parsed_request.dataframe_records")))
     final_df_with_requests = df_exploded.select(
-        [
-            F.col(f"record.{col.name}").alias(col.name)
-            for col in request_schema["dataframe_records"].dataType.elementType.fields
-        ]
+        [F.col(f"record.{col.name}").alias(col.name) for col in request_schema["dataframe_records"].dataType.elementType.fields]
     )
-    final_df_with_requests = final_df_with_requests.withColumn("model_name", (F.lit("hotel_reservations")))
 
     df_parsed = inf_table.withColumn("parsed_response", F.from_json(F.col("response"), response_schema))
-    final_df_with_response = df_parsed.select(
-        F.col("parsed_response.Client_ID").alias("Client_ID"),
-        F.col("parsed_response.Proba").alias("Proba"),
-        F.col("parsed_response.Comment").alias("Comment"),
+    df_exploded = df_parsed.withColumn("record", F.explode(F.col("parsed_response.predictions")))
+    final_df_with_response = df_exploded.select(
+        F.col("record.Client_ID").alias("Client_ID"),
+        F.col("record.Proba").alias("Proba"),
+        F.col("record.Comment").alias("Comment"),
         F.col("parsed_response.databricks_output.trace").alias("trace"),
-        F.col("parsed_response.databricks_output.databricks_request_id").alias("databricks_request_id"),
+        F.col("parsed_response.databricks_output.databricks_request_id").alias("databricks_request_id")
     )
 
-    main_df = spark.sql("SELECT request_date, request_time  FROM mlops_dev.olalubic.`model-serving-payload_payload`")
-
-    main_df = main_df.withColumn("row_id", monotonically_increasing_id())
-    final_df_with_response = final_df_with_response.withColumn("row_id", monotonically_increasing_id())
-    final_df_with_requests = final_df_with_requests.withColumn("row_id", monotonically_increasing_id())
-
-    result_df = (
-        main_df.join(final_df_with_response, on="row_id", how="inner")
-        .join(final_df_with_requests.drop("Client_ID"), on="row_id", how="inner")
-        .drop("row_id")
+    main_df = spark.sql(
+        f"SELECT request_date, request_time, databricks_request_id  FROM mlops_dev.olalubic.`model-serving-payload_payload`"
     )
 
-    train = spark.sql("SELECT Client_ID, booking_status FROM mlops_dev.olalubic.train_set")
-    test = spark.sql("SELECT Client_ID, booking_status  FROM mlops_dev.olalubic.test_set")
+    window = Window.partitionBy("Client_ID").orderBy("Client_ID")
+    final_df_with_response = final_df_with_response.withColumn("rn", row_number().over(window)).filter("rn = 1").drop("rn")
+
+
+    final_df_with_requests = final_df_with_requests.withColumn("rn", row_number().over(window)).filter("rn = 1").drop("rn")
+
+    result_df = final_df_with_requests \
+    .join(final_df_with_response, on="Client_ID", how="left")
+ 
+    result_df = result_df \
+    .join(main_df, on="databricks_request_id", how="left")
+
+    train = spark.sql(
+    f"SELECT Client_ID, booking_status FROM mlops_dev.olalubic.train_set"
+    )
+    test = spark.sql(
+        f"SELECT Client_ID, booking_status  FROM mlops_dev.olalubic.test_set"
+    )
+
     combined_df = test.unionByName(train)
+    combined_df = combined_df.dropDuplicates(["Client_ID"])
 
-    result_df = result_df.join(combined_df, on="Client_ID", how="inner")
+    result_df = result_df \
+    .join(combined_df, on="Client_ID", how="left")
 
-    result_df = result_df.dropna(subset=["booking_status", "prediction"])
+    result_df = result_df.dropna(subset=["booking_status", "Proba"])
     result_df = result_df.withColumn("timestamp", F.col("request_time"))
     result_df = result_df.withColumn("prediction_nr", when(col("Proba") > 0.5, 1).otherwise(0))
     result_df = result_df.withColumn(
         "booking_status_nr",
         when(col("booking_status") == "Canceled", 1).when(col("booking_status") == "Not_Canceled", 0),
     )
+    result_df = result_df.withColumn("model_name", lit("hotel_reservations"))
 
-    # hotel_reservations_features = spark.table(f"{config.catalog_name}.{config.schema_name}.hotel_reservations_features")
-
-    # df_final_with_features = df_final_with_status.join(hotel_reservations_features, on="Client_ID", how="left")
-
-    df_final_with_features.write.format("delta").mode("append").saveAsTable(
+    result_df.write.format("delta").mode("append").saveAsTable(
         f"{config.catalog_name}.{config.schema_name}.model_monitoring"
     )
 
